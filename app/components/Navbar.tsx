f0aad6745e40aea8564cfe2349bbc48b2c524e5e
manos <manos_xatzidakis1993@yahoo.gr>
2026-06-01 23:33:05 +0300

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import compass from "../../public/compass.png";
+import human from "../../public/human.png";
+export default function Navbar() {
+  return (
+    <div className="flex justify-between shadow-xl p-5">
+      <div className="flex row gap-5">
+        <Image src={compass} alt="" className="w-12 h-12" />
+        <div className="column text-center">
+          <h1 className="text-4xl font-bold">Wanderspots </h1>
+          <p className="text-xl">Discover & share amazing places</p>
+        </div>
+      </div>
+      <div className=" flex gap-5 ">
+        <button className="text-2xl cursor-pointer">Log in</button>
+        <div></div>
+        <button className="text-2xl  cursor-pointer bg-blue-400 rounded-4xl items-center px-3 flex row gap-2 justify-center">
+          <Image src={human} alt="" className="w-6 h-6" />
+          <span>Sign up</span>
+        </button>
+      </div>
+    </div>
+  );
+}
