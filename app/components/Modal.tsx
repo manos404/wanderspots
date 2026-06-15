36d2a93de69046f7c68df59cfcccd90d7426b322
manos <manos_xatzidakis1993@yahoo.gr>
2026-06-15 22:46:04 +0300

    form resusability

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,8 +1,10 @@
 "use client";
-import { Lock, Mail, X } from "lucide-react";
+import { Lock, Mail, X, User } from "lucide-react";
 import { useActionState } from "react";
 import { isEmail, isNotEmpty, hasMinLength } from "../util/validation";
+
 export default function Modal() {
+  let check = false;
   function SigninAction(prevFormState, formData) {
     const email = formData.get("email");
     const password = formData.get("password");
@@ -26,15 +28,36 @@ export default function Modal() {
   });
 
   return (
-    <dialog open className="m-auto w-100 h-100  rounded-4xl text-white">
+    <dialog open className="m-auto w-100    rounded-4xl text-white">
       <div className="p-3 pl-7 pb-7 bg-gradient-to-br from-blue-600 rounded-t-lg to-purple-600">
         <X className="ml-auto" />
-        <h1 className="text-2xl font-bold  ">Welcome Back!</h1>
-        <p className="text-sm pt-2">Sign in to continue</p>
+        <h1 className="text-2xl font-bold  ">
+          {check ? "Create Account" : "Welcome Back!"}
+        </h1>
+        <p className="text-sm pt-2">
+          {check ? "Join our community" : "Sign in to continue"}
+        </p>
       </div>
       <form action={formAction}>
         <div className=" flex flex-col p-7   text-gray-400">
-          <label htmlFor="email">Email</label>
+          {check && (
+            <>
+              <label htmlFor="email">Name</label>
+              <div className="relative mt-2">
+                <User className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
+
+                <input
+                  type="name"
+                  name="name"
+                  placeholder="Your name"
+                  className="w-full h-10 rounded-xl border border-gray-300 pl-10 pr-3 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                />
+              </div>
+            </>
+          )}
+          <label htmlFor="email" className="mt-2">
+            Email
+          </label>
           <div className="relative mt-2">
             <Mail className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
 
@@ -70,9 +93,18 @@ export default function Modal() {
         </div>
 
         <div className="flex justify-center   mx-5 h-12">
-          <button className=" bg-gradient-to-br from-blue-600   rounded-2xl to-purple-600  w-[80%] hover:from-blue-700 hover:to-purple-700 transition-all">
-            Sign in
+          <button className=" bg-gradient-to-br from-blue-600 rounded-2xl to-purple-600  w-[80%] hover:from-blue-700 hover:to-purple-700 transition-all">
+            {check ? "Sign up" : "Sign in"}
           </button>
+        </div>
+        <div className=" mt-2 flex flex-row gap-2 justify-center items-baseline">
+          <p className="text-gray-600 text-sm  ">
+            {check ? "Already have an acount?" : "Don't have an account?"}
+          </p>
+
+          <p className="text-blue-600 text-center">
+            {check ? "sign in" : "sign up"}
+          </p>
         </div>
       </form>
     </dialog>
