1318347af56449b8a9e0971028441f29c4dcf0c5
manos <manos_xatzidakis1993@yahoo.gr>
2026-06-25 22:45:42 +0300

    map/grid buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,60 @@
+"use client";
 import Card from "../components/Card";
 import { Spot } from "./types/spot";
 import { mockSpots } from "./data";
 import Modal from "../components/Modal";
+import { LayoutGrid, Map, Search } from "lucide-react";
+import { useState } from "react";
 export default function Home() {
+  const [activeButton, setActiveButton] = useState("map");
+
   return (
     <>
-      <div className=" flex gap-5 mx-30 my-10 items-center">
-        <p>6 amazing spots discovered</p>
-        <div className="flex flex-row gap-3 p-1 px-5 rounded-2xl bg-gray-200">
-          <button>Map</button>
-          <button>Grid</button>
+      <div className=" flex justify-between mx-30 items-center">
+        <div className="flex gap-5  my-10 items-center">
+          <p>6 amazing spots discovered</p>
+          <div className="flex flex-row gap-3   px-1 rounded-lg bg-gray-100 text-sm p-1">
+            <button
+              className={`flex gap-1 items-center rounded-md  px-2 py-1.5 ${
+                activeButton === "map" ? "text-blue-500 bg-white" : ""
+              }`}
+              onClick={() => setActiveButton("map")}
+            >
+              <Map size={18} /> <span>Map</span>
+            </button>
+            <button
+              className={`flex gap-1 items-center rounded-md px-2 py-1.5 ${
+                activeButton === "grid" ? "text-blue-500 bg-white" : ""
+              }`}
+              onClick={() => setActiveButton("grid")}
+            >
+              <LayoutGrid size={18} /> <span>Grid</span>
+            </button>
+          </div>
+        </div>
+        <div className="relative w-full max-w-sm">
+          <Search
+            size={18}
+            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"
+          />
+          <input
+            type="text"
+            placeholder="Search city or location..."
+            className="w-full rounded-md border border-gray-300 py-2 pl-10 pr-4 outline-none  focus:border-2 focus:border-blue-500"
+          />
         </div>
       </div>
-      <div className=" grid grid-cols-3 mx-30   gap-10">
-        <Card spot={mockSpots[0]} />
-        <Card spot={mockSpots[1]} />
-        <Card spot={mockSpots[2]} />
-        <Card spot={mockSpots[3]} />
-        <Card spot={mockSpots[4]} />
-        <Card spot={mockSpots[5]} />
-        <Modal />
-      </div>
+      {activeButton === "grid" && (
+        <div className=" grid grid-cols-3 mx-30   gap-10">
+          <Card spot={mockSpots[0]} />
+          <Card spot={mockSpots[1]} />
+          <Card spot={mockSpots[2]} />
+          <Card spot={mockSpots[3]} />
+          <Card spot={mockSpots[4]} />
+          <Card spot={mockSpots[5]} />
+          <Modal />
+        </div>
+      )}
     </>
   );
 }
